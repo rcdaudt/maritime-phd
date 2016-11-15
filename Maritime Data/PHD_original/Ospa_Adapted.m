1718d153165c359e2d39648011c7e57fe483ed53
oleksiiL <leonovich_68@mail.ru>
2016-11-15 00:25:03 +0000

    updated ospa function

--- Maritime Data/PHD_original/Ospa_Adapted.m
+++ Maritime Data/PHD_original/Ospa_Adapted.m
@@ -1,13 +1,16 @@
-function ospa = Ospa_Adapted(data, gt, cutoff_c, order_p)
-ospa = zeros(1,size(data,1));
-for i = 1:size(data,1)
-    Nobj_data = size(data{i},1);
-    Nobj_gt = size(gt{i},1);    
-    X = [data{i}'; zeros(2,Nobj_data)];
-    Y = [gt{i}'; zeros(2,Nobj_gt)];
-    ospa(i) = ospa_dist(X, Y, cutoff_c, order_p); 
-
+function ospa = Ospa_Adapted(gmm_u, data, cutoff_c, order_p)
+ind4X = {gmm_u.w}.'; 
+nonZero = find([ind4X{:}] > 0.05);
+X = {gmm_u.m}.'; 
+X = X(nonZero);
+%fix coordinates-velocities positions of X
+for i = 1 : size(X,1)
+    X{i} = [X{i}(1) X{i}(3) X{i}(2) X{i}(4)] ;
 end
+X = cell2mat(X)';
+%add fake velocities for Y    
+Y = [data'; zeros(2,size(data,1)) ]   ;
+ospa = ospa_dist(X, Y, 1, 1);
 end
 
 
--- Maritime Data/PHD_original/RUN_PHD.m
+++ Maritime Data/PHD_original/RUN_PHD.m
@@ -26,9 +26,9 @@ end
 addpath '..'
 %[data,gt,trajectories] = simulator(simfolder,cst,0);
 [data,gt,trajectories] = dtSim(simfolder,cst,0);
-ospa = Ospa_Adapted(data, gt, 1, 1);
-figure(); plot(ospa);
-title('Ospa metric');
+% ospa = Ospa_Adapted(data, gt, 1, 1);
+% figure(); plot(ospa);
+% title('Ospa metric');
 %% create initial Gaussian structure
 gaussian = struct('w', 0,...                    % weight
                   'm', zeros(cst.Nx,1),...      % mean
@@ -39,12 +39,28 @@ gmm_u = repmat(gaussian,cst.gmmax,1);
 isactive = zeros(1,cst.gmmax);
 
 %% The main loop
+ospa = zeros(1,size(data,1));
+
 for tt=1:cst.tmax
     [gmm_p,isactive] = PHD_prediction(gmm_u,isactive,cst);
     ind_p = find(isactive);
     [gmm_u,~,~,isactive] = PHD_update(gmm_p,data{tt},isactive,cst);
     ind_u = find(isactive);
+    ospa(tt) = Ospa_Adapted(gmm_u, data{tt}, 1, 1);
+%         ind4X = {gmm_u.w}.'; 
+%         nonZero = find([ind4X{:}] ~= 0);
+%         X = {gmm_u.m}.'; 
+%         X = X(nonZero);
+%         %fix coordinates-velocities positions of X
+%         for i = 1 : size(X,1)
+%             X{i} = [X{i}(1) X{i}(3) X{i}(2) X{i}(4)] ;
+%         end
+%             X = cell2mat(X)';
+%         Y = [data{tt}'; zeros(2,size(data{tt},1)) ]   ;
+%         ospa(tt) = ospa_dist(X, Y, 1, 1);
+    
     fprintf('time %3.d: #targets=%d, #meas=%d, pred - %3.d comp, mu=%.4g, update - %3.d comp, mu=%.4g \n',...
         tt,size(gt{tt},1),size(data{tt},1), length(ind_p),sum([gmm_p(ind_p).w]),length(ind_u),sum([gmm_u(ind_u).w]));
     plotGM(data{tt},gt{tt},trajectories,gmm_u,cst,tt);
 end
+figure(); plot(ospa); title('Ospa metric'); grid on;
